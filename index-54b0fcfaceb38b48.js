5c7647666de8d07acdcabe7096b380a675f8e607
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-30 15:46:25 -0500

    :rocket: Update 2

--- index-54b0fcfaceb38b48.js
+++ index-54b0fcfaceb38b48.js
@@ -259,7 +259,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-4e6cfbdebc000e72_bg.wasm', import.meta.url);
+        input = new URL('index-54b0fcfaceb38b48_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
