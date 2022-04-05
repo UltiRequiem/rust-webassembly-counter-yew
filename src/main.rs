4ab2d5515dcdbaf910775142e34602518066d110
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-05 09:14:45 -0500

    :recycle: Updater

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use wasm_bindgen::prelude::*;
 use web_sys::{console, HtmlInputElement};
 use yew::prelude::*;
-use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
@@ -29,31 +29,6 @@ fn app() -> Html {
         }
     };
 
-    let input_node_ref = use_node_ref();
-
-    let input_value_handle = use_state(String::default);
-    let input_value = (*input_value_handle).clone();
-
-    console::log_1(&format!("Input is {}", input_value).into());
-
-    let onchange = {
-        let input_node_ref = input_node_ref.clone();
-
-        let counter = counter.clone();
-
-        Callback::from(move |_| {
-            let input = input_node_ref.cast::<HtmlInputElement>();
-
-            if let Some(input) = input {
-                let value = input.value();
-
-                input_value_handle.set(value);
-
-                // counter.set(*value.parse::<i32>().unwrap_or(0));
-            }
-        })
-    };
-
     console::log_1(&"Hello using web-sys".into());
 
     html! {
@@ -61,19 +36,10 @@ fn app() -> Html {
            <p>
                 <b>{ " Current value: " }</b>
                 { *counter }
-            </p>
+           </p>
+
             <button onclick={increment_value}>{ "Increment value" }</button>
             <button onclick={decrement_value}>{ "Decrement value" }</button>
-
-             <label for="my-input">
-                { "Hack the current value:" }
-                <input ref={input_node_ref}
-                    {onchange}
-                    id="my-input"
-                    type="text"
-                    value={input_value}
-                />
-            </label>
         </main>
     }
 }
